0bd0da8fe713ec2ef34021e2f0745efb9f5f4905
Lee Wise <theleewise@gmail.com>
2020-12-05 11:24:06 -0600

    New custom hook for data fetching

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+// eslint-disable-next-line import/prefer-default-export
+export const useFetch = (initialUrl, initialParams = {}, skip = false) => {
+  const [url, updateUrl] = useState(initialUrl);
+  const [params, updateParams] = useState(initialParams);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [refetchIndex, setRefetchIndex] = useState(0);
+  const queryString = Object.keys(params).map((key) => (
+    `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  )).join('&');
+  const refetch = () => setRefetchIndex((prevRefetchIndex) => prevRefetchIndex + 1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (skip) return;
+      setIsLoading(true);
+      try {
+        const response = await fetch(`${url}${queryString}`);
+        const result = await response.json();
+        if (response.ok) {
+          setData(result);
+        } else {
+          setHasError(true);
+          setErrorMessage(result);
+        }
+      } catch (err) {
+        setHasError(true);
+        setErrorMessage(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [url, params, refetchIndex]);
+
+  return {
+    data, isLoading, hasError, errorMessage, updateUrl, updateParams, refetch,
+  };
+};
