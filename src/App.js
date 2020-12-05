42b02cef819d8900b3e333e44ea72ca690fe1328
Lee Wise <theleewise@gmail.com>
2020-12-05 00:49:11 -0600

    Fixes for linter & create initial form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="form-group">
+        <label htmlFor="systemCapacityInput">Nameplate Capacity (kW).</label>
+        <input id="systemCapacityInput" type="range" min="0.05" max="500000" step="10" className="form-control-range" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="moduleTypeInput">Module Type</label>
+        <select className="form-control" id="moduleTypeInput">
+          <option value="0">Standard</option>
+          <option value="1">Premium</option>
+          <option value="2">Thin film</option>
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="systemLossesInput">System Losses (percent)</label>
+        <input type="range" min="-5" max="99" step="1" className="form-control-range" id="lossesInput" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="arrayTypeInput">Array Type</label>
+        <select className="form-control" id="arrayTypeInput">
+          <option value="0">Fixed - Open Rack</option>
+          <option value="1">Fixed - Roof Mounted</option>
+          <option value="2">1-Axis</option>
+          <option value="3">1-Axis Backtracking</option>
+          <option value="4">2-Axis</option>
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="titleInput">Title Angle (degrees)</label>
+        <input type="range" min="0" max="99" step="1" className="form-control-range" id="titleInput" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="azimuthAngleInput">Azimuth Angle (degrees)</label>
+        <input type="range" min="0" max="99" step="1" className="form-control-range" id="azimuthAngleInput" />
+      </div>
     </div>
   );
 }
-
-export default App;
